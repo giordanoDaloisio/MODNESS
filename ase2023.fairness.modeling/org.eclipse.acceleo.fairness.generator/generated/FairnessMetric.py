92f442c7a53817de43a3afb4094e3bcab24f6c00
GIordano <giordano.daloisio@gmail.com>
2023-04-28 17:36:49 +0200

    fixed acceleo template

--- ase2023.fairness.modeling/org.eclipse.acceleo.fairness.generator/generated/FairnessMetric.py
+++ ase2023.fairness.modeling/org.eclipse.acceleo.fairness.generator/generated/FairnessMetric.py
@@ -1,5 +1,3 @@
-
-
 from aif360.metrics import ClassificationMetric
 from aif360.datasets import BinaryLabelDataset
 import pandas as pd
@@ -18,49 +16,62 @@ def logaritm(x):
 
 
 class FairnessMetric(ClassificationMetric):
-
-    def __init__(self, dataset_name: str, unprivileged_groups: dict, privileged_group: dict, true_label_name: str, predicted_label_name: str, positive_value: int):
-        self.df = pd.read_csv(dataset_name)
+    def __init__(
+        self,
+        dataset_name: str,
+        unprivileged_groups: dict,
+        privileged_group: dict,
+        true_label_name: str,
+        predicted_label_name: str,
+        positive_value: int,
+    ):
+        self.df = dataset_name
         self.dataset_true = BinaryLabelDataset(
             df=self.df.drop(columns=[predicted_label_name]),
             label_names=[true_label_name],
             protected_attribute_names=list(unprivileged_groups.keys()),
             favorable_label=positive_value,
-            unfavorable_label=1-positive_value)
+            unfavorable_label=1 - positive_value,
+        )
         self.dataset_pred = self.dataset_true.copy(deepcopy=True)
-        self.dataset_pred.labels = self.df[predicted_label_name].values.reshape(
-            -1, 1)
-        super().__init__(self.dataset_true, self.dataset_pred,
-                         unprivileged_groups=[unprivileged_groups], privileged_groups=[privileged_group])
+        self.dataset_pred.labels = self.df[predicted_label_name].values.reshape(-1, 1)
+        super().__init__(
+            self.dataset_true,
+            self.dataset_pred,
+            unprivileged_groups=[unprivileged_groups],
+            privileged_groups=[privileged_group],
+        )
 
-    def probability(self, object: str, condition: str = '') -> float:
+    def probability(self, object: str, condition: str = "") -> float:
         probability = self.df.query(object).shape[0] / self.df.shape[0]
-        if condition == '':
+        if condition == "":
             return probability
         else:
+            return (
+                self.df.query(condition + " and " + object).shape[0] / self.df.shape[0]
+            ) / probability
 
-            return (self.df.query(condition + ' and ' + object).shape[0]/self.df.shape[0]) / probability
-
-    def expected_value(self, column_name: str, condition: str = '') -> float:
+    def expected_value(self, column_name: str, condition: str = "") -> float:
         subdata = self.df[column_name]
-        if condition == '':
+        if condition == "":
             expected_value = 0
             for val in subdata.unique().tolist():
-                expected_value += val * \
-                    self.probability(column_name + ' == ' + str(val))
+                expected_value += val * self.probability(
+                    column_name + " == " + str(val)
+                )
             return expected_value
         else:
             expected_value = 0
             for val in subdata.unique().tolist():
-                expected_value += val * \
-                    self.probability(column_name + ' == ' +
-                                     str(val), condition)
+                expected_value += val * self.probability(
+                    column_name + " == " + str(val), condition
+                )
             return expected_value
 
-    def group_size(self, groupCondition: str = '', variable: str = ''):
-        if groupCondition != '':
+    def group_size(self, groupCondition: str = "", variable: str = ""):
+        if groupCondition != "":
             return self.df.query(groupCondition).shape[0]
-        elif variable != '':
+        elif variable != "":
             return len(self.df[variable].unique())
         return 0
 
--- ase2023.fairness.modeling/org.eclipse.acceleo.fairness.generator/generated/operators.py
+++ ase2023.fairness.modeling/org.eclipse.acceleo.fairness.generator/generated/operators.py
@@ -1,22 +1,19 @@
-
 from enum import Enum
-from abc import ABC
 
 
-class Operator(Enum, ABC):
+class Operator(Enum):
     pass
 
 
 class SingleOperator(Operator):
-    GREATER = '>'
-    MINOR = '<'
-    GREATER_EQUAL = '>='
-    MINOR_EQUAL = '<='
+    GREATER = ">"
+    MINOR = "<"
+    GREATER_EQUAL = ">="
+    MINOR_EQUAL = "<="
 
 
 class RangeOperator(Operator):
-    IN_INCLUDED = 'IN_INCLUDED'
-    IN_EXCLUDED = 'IN_EXCLUDED'
-    IN_LOWER_INCLUDED = 'IN_LOWER_INCLUDED'
-    IN_GREATER_INCLUDED = 'IN_GREATER_INCLUDED'
-
+    IN_INCLUDED = "IN_INCLUDED"
+    IN_EXCLUDED = "IN_EXCLUDED"
+    IN_LOWER_INCLUDED = "IN_LOWER_INCLUDED"
+    IN_GREATER_INCLUDED = "IN_GREATER_INCLUDED"
