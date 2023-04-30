5611bdf87b14babb8ab34bfec2eb0b8406affa1b
GIordano <giordano.daloisio@gmail.com>
2023-04-30 21:30:58 +0200

    WIP: adb7a0b fixed compas

--- ase2023.fairness.modeling/org.eclipse.acceleo.fairness.generator/generated/compas.py
+++ ase2023.fairness.modeling/org.eclipse.acceleo.fairness.generator/generated/compas.py
@@ -4,7 +4,7 @@ import pandas as pd
 from operators import SingleOperator
 
 
-file_path = "compas.csv"
+file_path = "data/compas.csv"
 predicted_label_name = "prediction"
 ground_truth_label_name = "two_year_recid"
 data = pd.read_csv(file_path)
@@ -30,7 +30,7 @@ metrics = FairnessMetric(
 
 
 print(metrics.statistical_parity_difference())
-if metrics.statistical_parity_difference() != threshold + tolerance_value:
+if abs(metrics.statistical_parity_difference()) > (threshold + tolerance_value):
     print("The dataset is biased")
 else:
     print("The dataset is fair")
