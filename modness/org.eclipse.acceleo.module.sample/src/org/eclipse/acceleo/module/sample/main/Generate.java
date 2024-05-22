4a8a55ac1122b2effa7bd905ef756a788e38be06
giordano.daloisio <giordano.daloisio@gmail.com>
2024-05-22 15:47:26 +0200

    update acceleo

--- modness/org.eclipse.acceleo.module.sample/src/org/eclipse/acceleo/module/sample/main/Generate.java
+++ modness/org.eclipse.acceleo.module.sample/src/org/eclipse/acceleo/module/sample/main/Generate.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     Obeo - initial API and implementation
  *******************************************************************************/
-package org.eclipse.acceleo.fairness.generator.main;
+package org.eclipse.acceleo.module.sample.main;
 
 import java.io.File;
 import java.io.IOException;
@@ -35,7 +35,7 @@ public class Generate extends AbstractAcceleoGenerator {
      *
      * @generated
      */
-    public static final String MODULE_FILE_NAME = "/org/eclipse/acceleo/fairness/generator/main/generate";
+    public static final String MODULE_FILE_NAME = "/org/eclipse/acceleo/module/sample/main/generate";
     
     /**
      * The name of the templates that are to be generated.
