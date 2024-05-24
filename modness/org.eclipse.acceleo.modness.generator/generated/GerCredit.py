8caeaffa157b9cdc52a2b23844337e20f29d8525
giordano.daloisio <giordano.daloisio@gmail.com>
2024-05-24 13:14:27 +0200

    added generated files

---
+++ modness/org.eclipse.acceleo.modness.generator/generated/GerCredit.py
@@ -0,0 +1,37 @@
+from FairnessMetric import FairnessMetric, binarize
+
+import pandas as pd
+from operators import SingleOperator
+
+
+file_path = "data/german.csv"
+predicted_label_name = "prediction"
+ground_truth_label_name = "credit"
+data = pd.read_csv(file_path)
+
+
+dataset_unprivileged_group = {"sex": 0}
+dataset_privileged_group = {"sex": 1}
+dataset_positive_outcome = 1
+
+
+threshold = 0.0
+
+tolerance_value = 0.2
+
+
+metrics = FairnessMetric(
+    data,
+    dataset_unprivileged_group,
+    dataset_privileged_group,
+    ground_truth_label_name,
+    predicted_label_name,
+    dataset_positive_outcome,
+)
+
+
+print(metrics.equal_opportunity_difference())
+if abs(metrics.equal_opportunity_difference()) > threshold + tolerance_value:
+    print("Biased")
+else:
+    print("Fair")
---
+++ modness/org.eclipse.acceleo.modness.generator/generated/operators.py
@@ -0,0 +1,21 @@
+
+from enum import Enum
+
+
+class Operator(Enum):
+    pass
+
+
+class SingleOperator(Operator):
+    GREATER = '>'
+    MINOR = '<'
+    GREATER_EQUAL = '>='
+    MINOR_EQUAL = '<='
+
+
+class RangeOperator(Operator):
+    IN_INCLUDED = 'IN_INCLUDED'
+    IN_EXCLUDED = 'IN_EXCLUDED'
+    IN_LOWER_INCLUDED = 'IN_LOWER_INCLUDED'
+    IN_GREATER_INCLUDED = 'IN_GREATER_INCLUDED'
+
